e0bd035393ac0c2ac6c9d200c9d8e4cf62e3ebb6
Uğur Şenaslan <senaslanugur@gmail.com>
2023-10-11 17:58:19 +0300

    logger js

---
+++ logger.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+// Initialize an empty JSON array to store test results
+let testResults = [];
+
+function logTestResult(testName, isPassed) {
+  const result = {
+    testName: testName,
+    isPassed: isPassed,
+    timestamp: new Date().toLocaleString(),
+  };
+  testResults.push(result);
+
+  if (isPassed) {
+    console.log(`✅ ${testName} - Passed`);
+  } else {
+    console.error(`❌ ${testName} - Failed`);
+  }
+}
+
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+};
+
+
+module.exports = {logTestResult, sleep, testResults}
--- test.js
+++ test.js
@@ -1,30 +1,17 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const fs = require('fs');
+const logger = require("./logger.js")
 
-// Initialize an empty JSON array to store test results
-let testResults = [];
+async function login(driver) {
+  await driver.get('https://www.saucedemo.com/');
+  logger.logTestResult('Open URL', true);
 
-function logTestResult(testName, isPassed) {
-  const result = {
-    testName: testName,
-    isPassed: isPassed,
-    timestamp: new Date().toLocaleString(),
-  };
-  testResults.push(result);
-
-  if (isPassed) {
-    console.log(`✅ ${testName} - Passed`);
-  } else {
-    console.error(`❌ ${testName} - Failed`);
-  }
+  await driver.findElement(By.id('user-name')).sendKeys('standard_user');
+  await driver.findElement(By.id('password')).sendKeys('secret_sauce');
+  await driver.findElement(By.id('login-button')).click();
+  logger.logTestResult('Login', true);
 }
-
-
-const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
-};
-
 
 async function run() {
   const driver = await new Builder()
@@ -33,39 +20,32 @@ async function run() {
     .build();
 
   try {
-    await driver.get('https://www.saucedemo.com/');
-    logTestResult('Open URL', true);
-
-    await driver.findElement(By.id('user-name')).sendKeys('standard_user');
-    await driver.findElement(By.id('password')).sendKeys('secret_sauce');
-    await driver.findElement(By.id('login-button')).click();
-    logTestResult('Login', true);
-
+    await login(driver); // Call the login function
     await driver.findElement(By.className('btn_primary')).click();
-    logTestResult('Add to Cart', true);
+    logger.logTestResult('Add to Cart', true);
 
     await driver.findElement(By.id('shopping_cart_container')).click();
-    logTestResult('View Shopping Cart', true);
+    logger.logTestResult('View Shopping Cart', true);
 
     // Add a new test step: Log out
     await driver.findElement(By.className('bm-burger-button')).click();
-    await sleep(3000);
+    await logger.sleep(3000);
     await driver.findElement(By.id('logout_sidebar_link')).click();
-    logTestResult('Log Out', true);
+    logger.logTestResult('Log Out', true);
 
     const screenshot = await driver.takeScreenshot();
     fs.writeFileSync('screenshot.png', screenshot, 'base64');
-    logTestResult('Take Screenshot', true);
+    logger.logTestResult('Take Screenshot', true);
 
   } catch (error) {
-    logTestResult('Exception', false);
+    logger.logTestResult('Exception', false);
     console.error(error);
   } finally {
     driver.quit();
   }
 
   // Write the test results to a JSON file
-  fs.writeFileSync('testResults.json', JSON.stringify(testResults, null, 2));
+  fs.writeFileSync('testResults.json', JSON.stringify(logger.testResults, null, 2));
 }
 
 run();
