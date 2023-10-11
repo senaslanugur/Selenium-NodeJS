6aa67992f3ab70c03d8e0e162d35105de405c494
Uğur Şenaslan <senaslanugur@gmail.com>
2023-10-11 18:15:08 +0300

    test
    test

---
+++ common.js
@@ -0,0 +1,26 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const fs = require('fs');
+const logger = require("./logger.js")
+
+
+async function login(driver) {
+  await driver.get('https://www.saucedemo.com/');
+  logger.logTestResult('Open URL', true);
+
+  await driver.findElement(By.id('user-name')).sendKeys('standard_user');
+  await driver.findElement(By.id('password')).sendKeys('secret_sauce');
+  await driver.findElement(By.id('login-button')).click();
+  logger.logTestResult('Login', true);
+}
+
+async function logout(driver) {
+    // Add a new test step: Log out
+    await driver.findElement(By.className('bm-burger-button')).click();
+    await logger.sleep(3000);
+    await driver.findElement(By.id('logout_sidebar_link')).click();
+    logger.logTestResult('Log Out', true);
+}
+
+
+module.exports = {login, logout}
--- test.js
+++ test.js
@@ -2,16 +2,8 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const fs = require('fs');
 const logger = require("./logger.js")
+const common = require("./common.js")
 
-async function login(driver) {
-  await driver.get('https://www.saucedemo.com/');
-  logger.logTestResult('Open URL', true);
-
-  await driver.findElement(By.id('user-name')).sendKeys('standard_user');
-  await driver.findElement(By.id('password')).sendKeys('secret_sauce');
-  await driver.findElement(By.id('login-button')).click();
-  logger.logTestResult('Login', true);
-}
 
 async function run() {
   const driver = await new Builder()
@@ -20,19 +12,18 @@ async function run() {
     .build();
 
   try {
-    await login(driver); // Call the login function
+    await common.login(driver); // Call the login function
+
+
     await driver.findElement(By.className('btn_primary')).click();
     logger.logTestResult('Add to Cart', true);
 
+    
     await driver.findElement(By.id('shopping_cart_container')).click();
     logger.logTestResult('View Shopping Cart', true);
 
-    // Add a new test step: Log out
-    await driver.findElement(By.className('bm-burger-button')).click();
-    await logger.sleep(3000);
-    await driver.findElement(By.id('logout_sidebar_link')).click();
-    logger.logTestResult('Log Out', true);
 
+    await common.logout(driver); // Call the logout function
     const screenshot = await driver.takeScreenshot();
     fs.writeFileSync('screenshot.png', screenshot, 'base64');
     logger.logTestResult('Take Screenshot', true);
